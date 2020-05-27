6ad53a9fd75359f559446b73e115be90a071a8a6
Ajay <akrish10@jcp.com>
2020-05-27 11:39:26 +0530

    URLcomponents issue fixed

--- src/main/java/com/example/myretail/repository/ProductRepositoryImpl.java
+++ src/main/java/com/example/myretail/repository/ProductRepositoryImpl.java
@@ -11,12 +11,13 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by akrish10 on 5/26/20.
@@ -30,34 +31,30 @@ public class ProductRepositoryImpl implements ProductRepository{
     private RestTemplate productRestTemplate;
 
     //To-Do: Replace url params
-    @Value("${product.rest.getProductsUrl:https://redsky.target.com/v2/pdp/tcin/13860428?excludes=taxonomy,price," +
-            "promotion," +
-            "bulk_ship,rating_and_review_reviews,rating_and_review_statistics,question_answer_statistics}")
+    @Value("${product.rest.getProductsUrl:https://redsky.target.com/v2/pdp/tcin/{productId}?excludes=taxonomy,price,promotion,bulk_ship,rating_and_review_reviews,rating_and_review_statistics,question_answer_statistics}")
     private String getProductsUrl;
 
 
     @Override
     public List<Product> getProductDetails(String productId) {
         log.debug("Inside repository");
+        ResponseEntity<ProductResponse> response = null;
         try {
-            MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
-            params.add("productId", productId);
-            ResponseEntity<ProductResponse> response = null;
-            response = productRestTemplate.exchange(getProductsUrl, HttpMethod.GET, null,
+            Map<String, String> urlParams = new HashMap<>();
+            urlParams.put("productId", productId);
+            UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(getProductsUrl);
+            String urlString = builder.buildAndExpand(urlParams).toUriString();
+            response = productRestTemplate.exchange(urlString, HttpMethod.GET, null,
                     new ParameterizedTypeReference<ProductResponse>() {
-                    },params);
+                    });
             if (response.getStatusCode().is2xxSuccessful()) {
                 return null;
             }
         } catch (Exception ex) {
-            HttpStatus httpStatusCode = HttpStatus.INTERNAL_SERVER_ERROR;
-            if (ex instanceof HttpStatusCodeException) {
-                HttpStatusCodeException httpException = (HttpStatusCodeException) ex;
-                httpStatusCode = httpException.getStatusCode();
-            }
-            log.error("Exception while calling segregate entity service", ex.getMessage());
-            if (httpStatusCode.value() == 404) {
-                log.debug("No Eligible entity present");
+            if(response.getStatusCode().is5xxServerError()){
+                log.debug("Internal server error");
+            } else if(response.getStatusCode().is4xxClientError()){
+                log.debug("4xx error");
             }
         }
         return null;
