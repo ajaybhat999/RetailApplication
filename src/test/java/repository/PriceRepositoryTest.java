36c5093eb7d771f258479d7bebd9a1ea3b2611e1
Ajay <akrish10@jcp.com>
2020-05-28 12:20:50 +0530

    Changes

--- src/test/java/repository/PriceRepositoryTest.java
+++ src/test/java/repository/PriceRepositoryTest.java
@@ -1,11 +1,43 @@
 package repository;
 
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.when;
+import static org.mockito.Matchers.any;
+
+import com.datastax.driver.core.querybuilder.Select;
+import com.example.myretail.model.PriceMapper;
+import com.example.myretail.repository.PriceRepositoryImpl;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.data.cassandra.core.CassandraOperations;
 
 /**
  * Created by akrish10 on 5/28/20.
  */
 @RunWith(MockitoJUnitRunner.class)
 public class PriceRepositoryTest {
+
+    @Mock
+    CassandraOperations cassandraOperations;
+
+    @InjectMocks
+    PriceRepositoryImpl priceRepository;
+
+
+    @Test
+    public void shouldFetchPriceIfEntryIsAvailableForProduct(){
+        PriceMapper priceMapper = PriceMapper.builder().productid("1234567").price("12").currency("USD").build();
+        when(cassandraOperations.selectOne(any(Select.class), any()))
+                .thenReturn(priceMapper);
+        PriceMapper resultPriceMapper = priceRepository.getPriceDetails("1234567");
+        assertNotNull(resultPriceMapper);
+        assertEquals(resultPriceMapper.getPrice(),"12");
+        assertEquals(resultPriceMapper.getCurrency(),"USD");
+        assertEquals(resultPriceMapper.getProductid(),"1234567");
+    }
 }
